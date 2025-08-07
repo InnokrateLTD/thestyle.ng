be727985c785de1aa30880034cbcfcbcb5b3d49e
Captressketh001 <shininglite2012@gmail.com>
2025-08-07 17:43:59 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-"use client"
-// import { useState } from "react";
+
 import Header from "@/app-components/landing-page/header";
 import NavLinks from "@/app-components/landing-page/nav-links";
 import CarouselContainer from "@/app-components/landing-page/carousel";
