82ced5f630a292a55e853a5bdccf9779bf44353c
Captressketh001 <shininglite2012@gmail.com>
2025-10-08 20:41:19 +0100

    removed _app.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,35 @@
 import type { Metadata } from "next";
 import { inter } from "@/lib/font"
-import "./globals.css";
+// import "./globals.css";
 import { Toaster } from "react-hot-toast";
+// export const metadata: Metadata = {
+//   title: "The Style Fashion Groups",
+//   description: "",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${inter.className}`}
+//       >
+//         {children}
+//         <Toaster/>
+//       </body>
+//     </html>
+//   );
+// }
+// app/layout.tsx
+import { SWRConfig } from "swr";
+import { apiRequest } from "@/lib/api";
+import "./globals.css";
+
+const fetcher = (url: string) => apiRequest(url, "GET");
+
 export const metadata: Metadata = {
   title: "The Style Fashion Groups",
   description: "",
@@ -9,16 +37,25 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        {children}
-        <Toaster/>
+      <body className={`${inter.className}`}>
+        <SWRConfig
+          value={{
+            fetcher,
+            onError: (error) => console.error("SWR Error:", error),
+            refreshInterval: 3000,
+            dedupingInterval: 2000,
+            revalidateOnFocus: true,
+            revalidateOnReconnect: true,
+          }}
+        >
+          {children}
+           <Toaster/>
+        </SWRConfig>
       </body>
     </html>
   );
